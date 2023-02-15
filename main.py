5f4beddd380260872d69fd44a4392b3d48d2174e
Triastrs <123349958+Triastrs@users.noreply.github.com>
2023-02-16 01:34:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 import requests
 from demonstration import *
 
-response = requests.get('https://api.npoint.io/e6729ddf2b5e8c56e84a'#данные с внешнего ресурса
+response = requests.get('https://api.npoint.io/e6729ddf2b5e8c56e84a') #данные с внешнего ресурса
 operations = json.loads(response.text)
 
 
